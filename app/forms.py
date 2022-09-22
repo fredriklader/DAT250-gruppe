e06b5a9c2a252b991998c56f96ab87d0b6b85a65
StianVR <stian00vr@gmail.com>
2022-09-22 13:03:12 +0200

    kanskje???

--- app/forms.py
+++ app/forms.py
@@ -7,8 +7,7 @@ from app import app, query_db
 #Nytt
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Length, DataRequired
-from wtforms_validators import AlphaNumeric
+from wtforms.validators import InputRequired, Length, DataRequired, Regexp
 
 
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -20,6 +19,27 @@ from wsgiref.validate import validator
 # login_manager = LoginManager(app)
 # login_manager.login_view="login"
 #UserMixin,
+
+text_validators = [
+    Regexp(
+        '[a-zA-Z0-9_.,!?-]$', 
+        message="Only letters and numbers, no special characters."),
+    Length(
+        min=3,
+        max=400,
+        message="Your message must be in between 3 and 400 characters."),
+]
+
+profile_validators = [
+    Regexp(
+        '[a-zA-Z0-9]',
+        message="Can only use letters and numbers."),
+    Length(
+        min=2,
+        max=50,
+        message="Your input needs to be between 2 and 50 characters"),
+]
+
 class LoginForm( FlaskForm):
     username = StringField('Username', render_kw={'placeholder': 'Username'})
     password = PasswordField('Password', render_kw={'placeholder': 'Password'})
@@ -38,25 +58,25 @@ class RegisterForm(FlaskForm):
 class IndexForm(FlaskForm):
     login = FormField(LoginForm)
     register = FormField(RegisterForm)
-#UserMixin,
+#UserMixin, 
 class PostForm(FlaskForm):
-    content = TextAreaField('New Post', render_kw={'placeholder': 'What are you thinking about?'})
+    content = TextAreaField('New Post', validators=[Length(max=300),Regexp('[a-zA-Z0-9]', message='Only letters and numbers')], render_kw={'placeholder': 'What are you thinking about?'})
     image = FileField('Image')
     submit = SubmitField('Post')
 
 class CommentsForm( FlaskForm):
-    comment = TextAreaField('New Comment', [DataRequired(), AlphaNumeric()], render_kw={'placeholder': 'What do you have to say?'})
+    comment = TextAreaField('New Comment', text_validators, render_kw={'placeholder': 'What do you have to say?'})
     submit = SubmitField('Comment')
 
 class FriendsForm( FlaskForm):
-    username = StringField('Friend\'s username', render_kw={'placeholder': 'Username'})
+    username = StringField('Friend\'s username', validators=[Length(min=4, max=15, message="Usernames contains between 4 and 15 characters!")], render_kw={'placeholder': 'Username'})
     submit = SubmitField('Add Friend')
 
 class ProfileForm( FlaskForm):
-    education = StringField('Education', render_kw={'placeholder': 'Highest education'})
-    employment = StringField('Employment', render_kw={'placeholder': 'Current employment'})
-    music = StringField('Favorite song', render_kw={'placeholder': 'Favorite song'})
-    movie = StringField('Favorite movie', render_kw={'placeholder': 'Favorite movie'})
-    nationality = StringField('Nationality', render_kw={'placeholder': 'Your nationality'})
+    education = StringField('Education', profile_validators, render_kw={'placeholder': 'Highest education'})
+    employment = StringField('Employment', profile_validators, render_kw={'placeholder': 'Current employment'})
+    music = StringField('Favorite song', profile_validators, render_kw={'placeholder': 'Favorite song'})
+    movie = StringField('Favorite movie', profile_validators, render_kw={'placeholder': 'Favorite movie'})
+    nationality = StringField('Nationality', profile_validators, render_kw={'placeholder': 'Your nationality'})
     birthday = DateField('Birthday')
     submit = SubmitField('Update Profile')
